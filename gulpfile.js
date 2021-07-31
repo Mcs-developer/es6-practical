9f33d07bcb837903e775683d916204569cfc66c6
Mcs-developer <marlsas0306@gmail.com>
2021-07-31 15:59:58 -0500

    Testing gulp file with js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const { series, src, dest } = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const del = require('del');
+const browserSync = require('browser-sync').create();
+const rollup = require('gulp-rollup');
 
 const clean = () => {
     return del(['dist/**'])
@@ -8,13 +12,38 @@ const clean = () => {
 
 const javascript = () => {
     return src('src/*.js')
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    // minificar
-    // uglify
-    .pipe(dest('dist/'));
+        .pipe(babel({
+            presets: ["@babel/preset-env"]
+        }))
+        // .pipe(concat('main.js', { newLine: ';' }))
+        .pipe(dest('dist/'));
 }
 
-exports.bundle = series(clean, javascript);
+const bundle = () => {
+    return src('dist/*.js')
+        .pipe(concat('main.js'))
+        .pipe(dest('dist/'))
+}
+
+const browserSyncServe = (cb) => {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+    cb();
+}
+
+const browserSyncReload = (cb) => {
+    browserSync.reload();
+    cb();
+}
+
+const watch = () => {
+    watch('src/index.html', browserSyncReload);
+    watch('src/*.js', series(clean, javascript, browserSyncReload));
+}
+
+exports.js = series(clean, javascript, bundle);
 
